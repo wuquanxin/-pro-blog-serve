a2252fcb085d6e69161de26c8863ab3f4b74fa9f
wuquanxin <729266380@qq.com>
2019-08-23 15:35:31 +0800

    后端页面

--- router/router-web.js
+++ router/router-web.js
@@ -2,6 +2,7 @@ const Router = require('koa-router');
 let route = new Router();
 let getDB = require('../tools/baseConnect');
 const crypto = require('crypto');
+const ObjectID = require('mongodb').ObjectID;
 
 
 route.prefix('/web');
@@ -13,7 +14,6 @@ route.post('/login', async(ctx, next) => {
     return crypto.randomBytes(length).toString('hex');
   };
   let { username, password } = ctx.request.body;
-  console.log(username, password);
   if (username == '' && password == '') {
     ctx.body = {
       code: 500,
@@ -51,18 +51,18 @@ route.post('/login', async(ctx, next) => {
     }
   }
 
-
-  //
 });
+// 获取用户信息
 route.post('/getUser', async(ctx, next) => {
   let coll = getDB().collection('user');
   let uid = ctx.session.user._id;
-  let result = await coll.findOne({_id: uid});
+  let result = await coll.findOne({ _id: uid });
   ctx.body = {
     code: 200,
-    information: {name: result.name,headImg: 'https://avatar-static.segmentfault.com/236/913/2369136975-5b620466b27c1_big64'}
+    information: { name: result.name, headImg: 'https://avatar-static.segmentfault.com/236/913/2369136975-5b620466b27c1_big64' }
   }
 });
+// 用户登出
 route.post('/loginOut', async(ctx, next) => {
   ctx.session = {}
   ctx.body = {
@@ -70,5 +70,90 @@ route.post('/loginOut', async(ctx, next) => {
     text: '成功注销！'
   }
 });
+// 添加标签
+route.post('/tagAdd', async(ctx, next) => {
+    let { tagname, tagdesc } = ctx.request.body;
+    if (tagname != '' && tagdesc != '') {
+      let coll = getDB().collection('tag');
+      await coll.insertOne({
+        'tagname': tagname,
+        'tagdesc': tagdesc
+      });
+      ctx.body = {
+        code: 200,
+        tagtext: {
+          'tagname': tagname,
+          'tagdesc': tagdesc
+        }
+      }
+    } else {
+      ctx.body = {
+        code: 500,
+        text: '数据不能为空！'
+      }
+    }
+  })
+  // 查询获取所有标签
+route.post('/tagFind', async(ctx, next) => {
+  let coll = getDB().collection('tag');
+  let tag = await coll.find();
+  let findtag = await tag.toArray();
+  ctx.body = {
+    code: 200,
+    taggroup: findtag
+  }
+})
+
+// 添加文章
+route.post('/articleAdd', async(ctx, next) => {
+    let { name, desc, content, tagid } = ctx.request.body;
+    if (name != '' && desc != '' && content != '' && tagid != '') {
+      let coll = getDB().collection('article');
+      await coll.insertOne({
+        'articlename': name,
+        'articledesc': desc,
+        'articlecontent': content,
+        'tagid': new ObjectID(tagid)
+      });
+      ctx.body = {
+        code: 200,
+        msg: '添加文章成功！'
+      }
+    } else {
+      ctx.body = {
+        code: 500,
+        text: '数据不能为空！'
+      }
+    }
+  })
+  // 查询获取文章列表
+route.post('/articleList', async(ctx, next) => {
+  let coll = getDB().collection('article');
+  let arcts = coll.aggregate([{
+    $lookup: { // 左连接a
+      from: "tag", // 关联到tag表
+      localField: 'tagid', // article 表关联的字段  数据类型统一
+      foreignField: "_id", // tag 表关联的字段  数据类型统一
+      as: "tags"
+    }
+  }, {
+    $unwind: { // 拆分子数组
+      path: "$tags"
+    }
+  },{
+    $group: { // 分组查询
+      _id: "$_id",
+      articlename: { $first: "$articlename" },
+      articledesc: { $first: "$articledesc" },
+      tagname: { $first: "$tags.tagname" },
+      // articlecontent: { $first: "$articlecontent" }
+    }
+  }]);
+  let findtag = await arcts.toArray();
+  ctx.body = {
+    code: 200,
+    taggroup: findtag
+  }
+})
 
 module.exports = route
